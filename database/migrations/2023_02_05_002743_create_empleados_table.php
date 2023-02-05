e22e8987c6b99e6e93fe8c84fc90379f9b29a3a4
Karen Flores <karemu.8910@gmail.com>
2023-02-04 19:35:46 -0600

    Migracion Empleados

--- database/migrations/2023_02_05_002743_create_empleados_table.php
+++ database/migrations/2023_02_05_002743_create_empleados_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('empleados', function (Blueprint $table) {
             $table->id();
+
+            $table->string('Nombre');
+            $table->string('ApellidoPaterno');
+            $table->string('ApellidoMaterno');
+            $table->string('Correo');
+            $table->string('Foto');
+
             $table->timestamps();
         });
     }
