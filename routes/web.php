b32760cd96613940acc092b99e7c3e457099ab5a
Karen Flores <karemu.8910@gmail.com>
2023-02-04 20:29:55 -0600

    Vistas de empleado

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/empleado', function () { //Ruta
+    return view('empleado.index'); //Carpeta.archivo
+});
